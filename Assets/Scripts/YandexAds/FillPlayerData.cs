00fa452a13d3118d4490ea0535b8c87242e918be
SashkaLevik <levik123@yandex.ru>
2023-02-22 09:38:37 +0300

    Init

--- Assets/Scripts/YandexAds/FillPlayerData.cs
+++ Assets/Scripts/YandexAds/FillPlayerData.cs
@@ -1,22 +1,37 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FillPlayerData : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _rank;
-    [SerializeField] private TMP_Text _name;
-    [SerializeField] private TMP_Text _score;
+    //[SerializeField] private TMP_Text _rank;
+    //[SerializeField] private TMP_Text _name;
+    //[SerializeField] private TMP_Text _score;
+    [SerializeField] private GameObject _rowPrefab;
+    [SerializeField] private Transform _rowParent;
 
     public void SetData(LeaderboardPlayers player)
     {
-        _rank.text = player.Rank.ToString();
+        GameObject newRow = Instantiate(_rowPrefab, _rowParent);
+        Text[] texts = newRow.GetComponentsInChildren<Text>();
+        texts[0].text = player.Rank.ToString();
 
         if (player.Rank == 0)
         {
-            _rank.text = "-";
+            texts[0].text = "-";
         }
 
-        _name.text = player.Name.ToString();
-        _score.text = player.Score.ToString();
+        texts[1].text = player.Name;
+        texts[2].text = player.Score.ToString();
+
+        //_rank.text = player.Rank.ToString();
+
+        //if (player.Rank == 0)
+        //{
+        //    _rank.text = "-";
+        //}
+
+        //_name.text = player.Name;
+        //_score.text = player.Score.ToString();
     }
 }
